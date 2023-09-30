5b61459e472b8f516c5bacd150ef9d0fbf7d3122
cayenneta <146495897+cayenneta@users.noreply.github.com>
2023-09-30 17:50:48 -0400

    finished

--- Python/Problems/Problem Set 2/Just setting up my twttr/twttr.py
+++ Python/Problems/Problem Set 2/Just setting up my twttr/twttr.py
@@ -2,7 +2,7 @@ def main():
 	UPPERCASE = ["A", "E", "I", "O", "U"]
 	text = input("Input: ")
 	for letter in text:
-		if (letter in UPPERCASE) or (letter.upper in UPPERCASE):
+		if (letter in UPPERCASE) or (letter.upper() in UPPERCASE):
 			text = text.replace(letter, "")
 	print(f"Output: {text}")
 if __name__ == '__main__':
