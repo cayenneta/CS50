36ee80fae27013d02ea3d2d9c4d356ab44ca61c9
cayenneta <146495897+cayenneta@users.noreply.github.com>
2023-10-01 01:04:20 -0400

    it wont wooorrk

--- Python/Problems/Problem Set 2/Vanity Plates/plates.py
+++ Python/Problems/Problem Set 2/Vanity Plates/plates.py
@@ -2,7 +2,7 @@ def is_valid(s):
 	first_digit = True
 	if not 2 <= len(s) <= 6:
 		return False
-	if not (s[0] and s[1]).isalpha():
+	if not s[:2].isalpha():
 		return False
 	for letter in s:
 		if not letter.isalpha() or letter.isdigit():
@@ -10,10 +10,9 @@ def is_valid(s):
 	for letter in s:
 		if letter.isdigit() and first_digit == True and letter == "0":
 			return False
-		elif letter.isdigit() and not (first_digit == True and letter == "0"):
-			first_digit = True
 		if letter.isdigit():
-			if not s[-1].isdigit:
+			first_digit = False
+			if not s[-1].isdigit():
 				return False
 	else:
 		return True
