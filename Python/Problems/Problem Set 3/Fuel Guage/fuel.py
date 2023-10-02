f22f7ff7b4cd7a460befe34161852a57c5eb713a
cayenneta <146495897+cayenneta@users.noreply.github.com>
2023-10-02 16:06:11 -0400

    dsd

--- Python/Problems/Problem Set 3/Fuel Guage/fuel.py
+++ Python/Problems/Problem Set 3/Fuel Guage/fuel.py
@@ -1,4 +1,17 @@
+def gas_checker(fuel):
+	fuel = input("Fraction: ")	
+	x,y = float(fuel.split("/"))
+	print(x + y)	
+	tank = (x/y)
+	if (tank) <= 0.1:
+		return "E"	
+	elif (tank) >= 0.9:
+		return "Y"
+
 def main():
-	pass
+	while True:
+		try:
+		except ValueError:
+			pass
 if __name__ == '__main__':
 	main()
