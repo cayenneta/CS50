20c66e9769bef28fa971f3854e5ef3ac012691dc
cayenneta <146495897+cayenneta@users.noreply.github.com>
2023-10-02 19:14:29 -0400

    this one sucked

--- Python/Problems/Problem Set 3/Fuel Guage/fuel.py
+++ Python/Problems/Problem Set 3/Fuel Guage/fuel.py
@@ -1,15 +1,20 @@
-def gas_checker(fuel):
-	tank = eval(fuel)
-	if (tank) <= 0.1:
-		return "E"	
-	elif (tank) >= 0.9:
-		return "F"
+import math
+
+bol = True
+while bol:
+	frac = input("Enter a fraction (x/y): ")
+	frac = frac.split("/")
+	if (frac[0].isnumeric() == False or frac[1].isnumeric() == False) or (int(frac[1]) < 0) or (int(frac[0]) > int(frac[1])):
+		print("Invalid input")
 	else:
-		return f"{int(tank*100)}%"
-def main():
-	try:
-		print(gas_checker(input("Fraction: ")))
-	except Exception as e:
-		main()
-if __name__ == '__main__':
-	main()
+		bol = False
+frac = [float(i) for i in frac]
+frac = frac[0] / frac[1]
+frac = frac*100
+frac = round(frac)
+if (frac >= 99):
+	print("F")
+elif (frac <= 1):
+	print("E")
+else:
+	print(f"{frac}%")
