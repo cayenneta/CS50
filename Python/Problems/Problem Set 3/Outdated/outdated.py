1552f2854801d50a7b75b3d1b7119afb9d89b7ff
cayenneta <146495897+cayenneta@users.noreply.github.com>
2023-10-03 13:58:32 -0400

    working on it

--- Python/Problems/Problem Set 3/Outdated/outdated.py
+++ Python/Problems/Problem Set 3/Outdated/outdated.py
@@ -1,3 +1,4 @@
+import sys
 months = [
     "january",
     "february",
@@ -15,19 +16,30 @@ months = [
 
 def main():
 	while True:
-		try:
-			us_date = input("Date: ")
-			if "/" in us_date:
-				parts = us_date.split("/")
-				return parts
-			elif "," in us_date:
-				parts = us_date.split(" ")
-				parts[1] = parts[1].replace(",","")
-				parts[0] = months.index(parts[0]) + 1
-				return parts
-		except Exception:
+		us_date = input("Date: ")
+		if "/" in us_date:
+			parts = us_date.split("/")
+			if any(char.isalpha() for string in parts for char in string):
+				continue
+			for i in range(len(parts)):
+				parts[i] = parts[i].replace(" ","")
+			for i in range(2):
+				if len(parts[i]) == 1:
+					parts[i] = f"0{parts[i]}"
+			if (not parts[0] <= 12) or (not parts[1] <= 31):
+				print(f"{parts[2]}-{parts[0]}-{parts[1]}")
+			sys.exit()
+		elif "," in us_date:
+			parts = us_date.split(" ")
+			parts[1] = parts[1].replace(",","")
+			parts[0] = months.index(parts[0].lower()) + 1
+			for i in range(2):
+				if len(parts[i]) == 1:
+					parts[i] = f"0{parts[i]}"
+			print(f"{parts[2]}-{parts[0]}-{parts[1]}")
+			sys.exit()
+		else:
 			pass
-	print(f"{parts[2]}-{parts[0]}-{parts[1]}")
 if __name__ == '__main__':
 	main()
  
