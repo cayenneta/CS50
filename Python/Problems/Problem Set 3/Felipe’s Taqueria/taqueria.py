043f8ed5ad1759307dbac29dd66520ac8da70060
cayenneta <146495897+cayenneta@users.noreply.github.com>
2023-10-03 11:09:27 -0400

    nearly done

--- Python/Problems/Problem Set 3/Felipe’s Taqueria/taqueria.py
+++ Python/Problems/Problem Set 3/Felipe’s Taqueria/taqueria.py
@@ -1,4 +1,22 @@
+menu = {
+    "Baja Taco": 4.00,
+    "Burrito": 7.50,
+    "Bowl": 8.50,
+    "Nachos": 11.00,
+    "Quesadilla": 8.50,
+    "Super Burrito": 8.50,
+    "Super Quesadilla": 9.50,
+    "Taco": 3.00,
+    "Tortilla Salad": 8.00
+}
 def main():
-	pass
+	price = float(0)
+	while True:
+		order = input("Item: ")
+		for key, value in menu.items():
+			if order.lower() == key.lower():
+				price += value
+				print(f"Total: ${price:.2f}")
+				break
 if __name__ == '__main__':
 	main()
