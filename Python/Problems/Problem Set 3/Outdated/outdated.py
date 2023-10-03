2b8f7815a45f0bce037455c4ce2c5d747dd2744a
cayenneta <146495897+cayenneta@users.noreply.github.com>
2023-10-03 14:31:06 -0400

    ONLY 2 ERRORS LEFT THIS COMMIT

--- Python/Problems/Problem Set 3/Outdated/outdated.py
+++ Python/Problems/Problem Set 3/Outdated/outdated.py
@@ -24,17 +24,23 @@ def main():
 			for i in range(len(parts)):
 				parts[i] = parts[i].replace(" ","")
 			for i in range(2):
-				if len(parts[i]) == 1:
+				if len(str(parts[i])) == 1:
 					parts[i] = f"0{parts[i]}"
-			if (not parts[0] <= 12) or (not parts[1] <= 31):
-				print(f"{parts[2]}-{parts[0]}-{parts[1]}")
+			if (not int(parts[0]) <= 12) or (not int(parts[1]) <= 31):
+				continue
+			print(f"{parts[2]}-{parts[0]}-{parts[1]}")
 			sys.exit()
 		elif "," in us_date:
 			parts = us_date.split(" ")
 			parts[1] = parts[1].replace(",","")
-			parts[0] = months.index(parts[0].lower()) + 1
+			try:
+				parts[0] = months.index(parts[0].lower()) + 1
+			except ValueError:
+				continue
+			if (not int(parts[0]) <= 12) or (not (int(parts[2]) <= 31)):
+				continue
 			for i in range(2):
-				if len(parts[i]) == 1:
+				if len(str(parts[i])) == 1:
 					parts[i] = f"0{parts[i]}"
 			print(f"{parts[2]}-{parts[0]}-{parts[1]}")
 			sys.exit()
