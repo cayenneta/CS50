1538bdb470d4978d1c45a777d7180d5905eb1df6
cayenneta <146495897+cayenneta@users.noreply.github.com>
2023-10-05 13:34:22 -0400

    idk what im doing man

--- Python/Problems/Problem Set 4/Guessing Game/game.py
+++ Python/Problems/Problem Set 4/Guessing Game/game.py
@@ -1,4 +1,23 @@
+import random
+
+def posint_checker(input, text):
+	while True:
+		try:
+			input = int(input(text))
+		except:
+			continue
+		if input > 0:
+			return input
+		else:
+			continue
+
+def generator():
+	posint_checker(level, "Level: ")
+
+def checker():
+	posint_checker(guess, "Guess: ")
+
 def main():
-	pass
+	generator()
 if __name__ == '__main__':
 	main()
