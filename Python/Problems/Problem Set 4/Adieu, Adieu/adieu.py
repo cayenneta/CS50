a77a1673c7ec8354ff26e841fd685359357a0b8b
cayenneta <146495897+cayenneta@users.noreply.github.com>
2023-10-05 11:46:04 -0400

    finished adieu and started game

--- Python/Problems/Problem Set 4/Adieu, Adieu/adieu.py
+++ Python/Problems/Problem Set 4/Adieu, Adieu/adieu.py
@@ -1,4 +1,21 @@
+import inflect
+p = inflect.engine()
+name_list = []
+
+def collect_names():
+	try:
+		while True:
+			input_name = input("Name: ")
+			name_list.append(input_name)
+	except EOFError:
+		return name_list
+
+def process_names():
+	names_ordered = p.join(name_list)
+	print(f"\nAdieu, adieu, to {names_ordered}")
+
 def main():
-	pass
+	collect_names()
+	process_names()
 if __name__ == '__main__':
 	main()
