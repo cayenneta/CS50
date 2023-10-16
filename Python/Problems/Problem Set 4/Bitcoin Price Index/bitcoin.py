017cb2e86af842c12916563988f1d00c49b6c62e
cayenneta <146495897+cayenneta@users.noreply.github.com>
2023-10-16 13:34:56 -0400

    finished bitcoin

--- Python/Problems/Problem Set 4/Bitcoin Price Index/bitcoin.py
+++ Python/Problems/Problem Set 4/Bitcoin Price Index/bitcoin.py
@@ -1,4 +1,29 @@
+import requests
+import sys
+
+def float_check():
+	try:
+		amount = float(sys.argv[1])
+	except IndexError:
+		sys.exit("Missing command-line argument")
+	except ValueError:
+		sys.exc_info("Command-line argument is not a number")
+	return amount
+
+def get_price():
+	bitcoin = requests.get('https://api.coindesk.com/v1/bpi/currentprice.json')
+	bitcoin = bitcoin.json()
+	rate = float(bitcoin["bpi"]["USD"]["rate_float"])
+	return rate
+
+def convert(amount, rate):
+	usd = amount * rate
+	return usd
+
 def main():
-	pass # TODO
+	amount = float_check()
+	rate = get_price()
+	usd = convert(amount, rate)
+	print(f"${usd:,.4f}")
 if __name__ == '__main__':
 	main()
